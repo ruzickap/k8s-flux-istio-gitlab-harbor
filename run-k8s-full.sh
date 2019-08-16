dcfe9f2acb9f20f51e9f84977f2344bdaa00f1a2
Petr Ruzicka <petr.ruzicka@gmail.com>
2019-08-16 12:38:58 +0200

    Adding scripts

---
+++ run-k8s-full.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -eu
+
+################################################
+# include the magic
+################################################
+test -s ./demo-magic.sh || curl --silent https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh > demo-magic.sh
+. ./demo-magic.sh
+
+################################################
+# Configure the options
+################################################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=600
+
+# Uncomment to run non-interactively
+export PROMPT_TIMEOUT=0
+
+# No wait
+export NO_WAIT=true
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+DEMO_PROMPT="${GREEN}➜ ${CYAN}$ "
+
+# hide the evidence
+#clear
+
+### Please run these commands before running the script
+
+# if [ -n "$SSH_AUTH_SOCK" ]; then
+#  docker run -it --rm -e USER="$USER" -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK -v $PWD:/mnt -v $HOME/.ssh:/root/.ssh:ro -v $HOME/.aws:/root/.aws ubuntu
+# else
+#  docker run -it --rm -e USER="$USER" -v $PWD:/mnt -v $HOME/.ssh:/root/.ssh:ro -v $HOME/.aws:/root/.aws ubuntu
+# fi
+# echo $(hostname -I) $(hostname) >> /etc/hosts
+# apt-get update -qq && apt-get install -qq -y curl git pv > /dev/null
+# cd /mnt
+
+# export LETSENCRYPT_ENVIRONMENT="production"  # Use with care - Let's Encrypt will generate real certificates
+# export MY_DOMAIN="mylabs.dev"
+
+# ./run-k8s-full.sh
+
+[ ! -d .git ] && git clone --quiet https://github.com/ruzickap/k8s-flux-knative-gitlab-harbor && cd k8s-flux-knative-gitlab-harbor
+
+sed docs/part-01/README.md \
+  -e '/^## Configure AWS/,/^Create policy allowing the cert-manager to change Route 53 settings./d' \
+| \
+sed -n '/^```bash.*/,/^```$/p' \
+| \
+sed \
+  -e 's/^```bash.*/\
+pe '"'"'/' \
+  -e 's/^```$/'"'"'/' \
+> README.sh
+
+
+if [ "$#" -eq 0 ]; then
+  source README.sh
+else
+  cat README.sh
+fi
