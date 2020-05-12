91663e8940e689d249ae2024aca504276da3b86a
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-05-12 06:19:32 +0200

    Add shellcheck + necessary changes in the scripts

--- run-k8s-part2.sh
+++ run-k8s-part2.sh
@@ -6,6 +6,7 @@ set -eu
 # include the magic
 ################################################
 test -s ./demo-magic.sh || curl --silent https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh > demo-magic.sh
+# shellcheck disable=SC1091
 . ./demo-magic.sh
 
 ################################################
@@ -15,7 +16,7 @@ test -s ./demo-magic.sh || curl --silent https://raw.githubusercontent.com/paxto
 #
 # speed at which to simulate typing. bigger num = faster
 #
-TYPE_SPEED=60
+export TYPE_SPEED=60
 
 # Uncomment to run non-interactively
 export PROMPT_TIMEOUT=0
@@ -29,7 +30,7 @@ export NO_WAIT=false
 # see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
 #
 #DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
-DEMO_PROMPT="${GREEN}➜ ${CYAN}$ "
+export DEMO_PROMPT="${GREEN}➜ ${CYAN}$ "
 
 # hide the evidence
 #clear
@@ -53,16 +54,11 @@ DEMO_PROMPT="${GREEN}➜ ${CYAN}$ "
 
 [ ! -d .git ] && git clone --quiet https://github.com/ruzickap/k8s-flux-istio-gitlab-harbor && cd k8s-flux-istio-gitlab-harbor
 
-sed -n '/^```bash$/,/^```$/p;/^-----$/p' docs/part-0{4,5}/README.md \
+sed -n "/^\`\`\`bash.*/,/^\`\`\`$/p;/^-----$/p" docs/part-0{4,5}/README.md \
 | \
 sed \
-  -e 's/^-----$/\
-p  ""\
-p  "################################################################################################### Press <ENTER> to continue"\
-wait\
-/' \
-  -e 's/^```bash.*/\
-pe '"'"'/' \
+  -e 's/^-----$/\np  ""\np  "################################################################################################### Press <ENTER> to continue"\nwait\n/' \
+  -e 's/^```bash.*/\npe '"'"'/' \
   -e 's/^```$/'"'"'/' \
   -e 's/^sleep /#sleep /' \
 > README.sh
@@ -79,12 +75,14 @@ if [ "$#" -eq 0 ]; then
   # ./run-k8s-part2.sh
 
   export MY_DOMAIN="mylabs.dev"
-  export EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID=$(awk -F\" "/AccessKeyId/ { print \$4 }" $HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN})
-  export EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY=$(awk -F\" "/SecretAccessKey/ { print \$4 }" $HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN})
-  kops export kubecfg ${USER}-k8s.${MY_DOMAIN} --state=s3://${USER}-kops-k8s --kubeconfig kubeconfig.conf
-  echo -e "\n*** ${MY_DOMAIN} | ${LETSENCRYPT_ENVIRONMENT} | ${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID} | ${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY} ***\n`kubectl --kubeconfig=./kubeconfig.conf cluster-info`"
+  EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID=$(awk -F\" "/AccessKeyId/ { print \$4 }" "$HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN}")
+  export EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID
+  EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY=$(awk -F\" "/SecretAccessKey/ { print \$4 }" "$HOME/.aws/${USER}-eks-cert-manager-route53-${MY_DOMAIN}")
+  export EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY
+  kops export kubecfg "${USER}-k8s.${MY_DOMAIN}" --state="s3://${USER}-kops-k8s" --kubeconfig kubeconfig.conf
+  echo -e "\n*** ${MY_DOMAIN} | ${LETSENCRYPT_ENVIRONMENT} | ${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID} | ${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY} ***\n$(kubectl --kubeconfig=./kubeconfig.conf cluster-info)"
 
-  if [ -z ${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID} ] || [ -z ${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY} ]; then
+  if [ -z "${EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID}" ] || [ -z "${EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY}" ]; then
     echo -e "\n*** One of the mandatory variables 'EKS_CERT_MANAGER_ROUTE53_AWS_ACCESS_KEY_ID' or 'EKS_CERT_MANAGER_ROUTE53_AWS_SECRET_ACCESS_KEY' is not set !!\n";
     exit 1
   fi
@@ -93,10 +91,11 @@ if [ "$#" -eq 0 ]; then
   set +eux
 
   echo -e "\n\n*** Press ENTER to start\n"
-  read A
+  read -r
 
   # hide the evidence
   clear
+  # shellcheck disable=SC1091
   source README.sh
 else
   cat README.sh
